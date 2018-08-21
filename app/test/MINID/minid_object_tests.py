a6602b77611880f4136bfe90fdaf5802cbd6af9b
Max Levinson <mlev71@users.noreply.github.com>
2018-08-21 14:30:06 -0400

    importing parameterized to minid_object_tests.py

--- app/test/MINID/minid_object_tests.py
+++ app/test/MINID/minid_object_tests.py
@@ -5,15 +5,17 @@ from nose.tools import assert_equal, assert_true
 from app.components.ezid_anvl import *
 from app.components.identifier_objects import Minid 
 
+from parameterized import parameterized
+
 minid_examples = ['ark:/57799/b90t23', 'ark:/57799/b90w82']
 
 @parameterized(minid_examples)
 def init_minid(identifier):
     try:
         minid_obj = Minid(identifier)
+        assert minid_obj is not None
     except OutOfPath as e:
         assert e.repsonse is not None
-    assert minid_obj is not None
 
 
 
